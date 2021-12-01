32fd6f531b09cab958c989ce7cd169e5fafc7df7
patrickgilsf <johnpatrickgilligan@gmail.com>
2021-11-30 19:18:12 -0800

    added secret env file and auto file creation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('./config');
 const rp = require('request-promise');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const createFile = require('create-file');
 
 
 var options = {
@@ -21,12 +22,26 @@ request(options, function (error, response, body) {
 	var zr = parsed.zoom_rooms;
 //this creates the list
 var newArr = [];
+var newStr = "";
 Object.entries(zr).forEach(
     ([key, value]) => {
 		if (value.status === "Offline") {
 			newArr.push(zr[key]);
-			console.log(zr[key].room_name);
+			newStr = newStr + zr[key].room_name + '\n'
 		}
 	}
 );
+//this creates a file with current date (sliced), within this codes' directory
+let date = new Date();
+let dateStr = date.toString().slice(0,10);
+let fileStr = './FileOutput/' + dateStr;
+
+
+
+createFile(fileStr, newStr, (err) => {
+	if(err) {
+		console.error(err);
+	}
+})
+
 });
